668b2024eaf19cef1c1f6f9089185d4dfa6dd752
IsiraAdithya <isiradabare1230@gmail.com>
2024-06-29 19:26:43 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function calc(){
+
+
+
+   let number1= document.getElementById("number1").value;
+   let number2 = document.getElementById("number2").value;
+   let lblOutput = document.getElementById("output");
+
+   lblOutput.innerHTML=( number1 + number2);
+
+}
