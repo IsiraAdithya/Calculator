a9df67d67cd66197172c0029f0a17de980a417db
IsiraAdithya <isiradabare1230@gmail.com>
2024-06-29 19:57:27 +0530

    added operations

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ function calc() {
 
     let number1 = Number(document.getElementById("number1").value);
     let number2 = Number(document.getElementById("number2").value);
-
-    
     let lblOutput = document.getElementById("output");
 
-    lblOutput.innerHTML = (number1 + number2);
+    let operation = document.getElementById("operation").value;
+
+    if (operation == "+") {
+        lblOutput.innerHTML = (number1 + number2);
+    }
+    else
+
+ lblOutput.innerHTML = (number1 - number2);
+    
+   
+
+    
+    
 
 }
