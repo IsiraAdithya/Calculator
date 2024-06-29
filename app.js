a3005e9535ea84fff4fa677438d9a7a2eb5e8271
IsiraAdithya <isiradabare1230@gmail.com>
2024-06-29 19:37:45 +0530

    change the types of number 1 and 2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-function calc(){
+function calc() {
 
 
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
 
-   let number1= document.getElementById("number1").value;
-   let number2 = document.getElementById("number2").value;
-   let lblOutput = document.getElementById("output");
+    
+    let lblOutput = document.getElementById("output");
 
-   lblOutput.innerHTML=( number1 + number2);
+    lblOutput.innerHTML = (number1 + number2);
 
 }
